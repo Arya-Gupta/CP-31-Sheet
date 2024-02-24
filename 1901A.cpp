531d5a640e90432673c84fd833abeaebd1796e9e
unknown <arya.gupta99999@gmail.com>
2024-02-24 20:46:20 +0530

    1901A

---
+++ 1901A.cpp
@@ -0,0 +1,14 @@
+// https://codeforces.com/problemset/problem/1901/A
+
+// #include <bits/stdc++.h>
+#include<iostream>
+using namespace std;
+ 
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        cout << "hello";
+    }
+    return 0; 
+}
